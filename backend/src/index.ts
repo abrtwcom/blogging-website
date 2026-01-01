65b6d2d0fa3a517972a09d8f1a87417544be88e3
abrtwcom <amitbartwal008@gmail.com>
2026-01-01 21:28:29 +0530

    Initial setup: schemas, routes, config for blogging website

---
+++ backend/src/index.ts
@@ -0,0 +1,32 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+app.post('/api/v1/user/signup', (c) => {
+  return c.json({message: 'User signed up successfully '})
+})
+app.post('/api/v1/user/signin', (c) => {
+  return c.json({message:'User sign in successfully'})
+}) 
+
+app.post('/api/v1/blog' ,(c) => {
+  return c.json({message:'User access blog'})
+  })
+
+app.put('/api/v1/blog' ,(c) => {
+  return c.json({message: "Write the blog"})
+})
+
+app.get('/api/v1/blog/:id' ,(c) =>{
+  const id = c.req.param('id')
+	return c.text('get blog route')
+})
+app.get('/api/v1/blog/bulk' , (c) => {
+	return c.text('signin route')
+})
+
+export default app ;
+
